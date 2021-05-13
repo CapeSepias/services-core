1bd5add2832456896057c202237c6cea838c7014
stephannv <3025661+stephannv@users.noreply.github.com>
2021-05-13 08:47:05 -0300

    WIP

---
+++ services/catarse/app/lib/shared/address_replicator.rb
@@ -0,0 +1,10 @@
+module Shared
+  class AddressReplicator
+    def self.by_id(address_id)
+      address = ::Shared::Address.find(address_id)
+      address_replica = address.dup
+      address_replica.save!
+      address_replica
+    end
+  end
+end
